e01370fb6c98497d2edb931030bc7f043fcbfece
pagadalamunikarthik-ux <pagadalamunikarthik@gmail.com>
2026-01-28 10:59:49 +0530

    Output

---
+++ EXP - 22.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+int main() {
+    int nb, np;
+    int block[20], process[20];
+    int allocation[20];
+    int i, j;
+
+    printf("Enter number of memory blocks: ");
+    scanf("%d", &nb);
+
+    printf("Enter size of each block:\n");
+    for (i = 0; i < nb; i++) {
+        scanf("%d", &block[i]);
+    }
+
+    printf("\nEnter number of processes: ");
+    scanf("%d", &np);
+
+    printf("Enter size of each process:\n");
+    for (i = 0; i < np; i++) {
+        scanf("%d", &process[i]);
+        allocation[i] = -1;   // Not allocated
+    }
+
+    // Best Fit Allocation
+    for (i = 0; i < np; i++) {
+        int best = -1;
+
+        for (j = 0; j < nb; j++) {
+            if (block[j] >= process[i]) {
+                if (best == -1 || block[j] < block[best]) {
+                    best = j;
+                }
+            }
+        }
+
+        if (best != -1) {
+            allocation[i] = best;
+            block[best] -= process[i];
+        }
+    }
+
+    printf("\nProcess No\tProcess Size\tBlock No\n");
+    for (i = 0; i < np; i++) {
+        printf("%d\t\t%d\t\t", i + 1, process[i]);
+        if (allocation[i] != -1)
+            printf("%d\n", allocation[i] + 1);
+        else
+            printf("Not Allocated\n");
+    }
+
+    return 0;
+}
